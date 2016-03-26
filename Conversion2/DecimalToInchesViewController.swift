a5536d3ef937d7e6ad80b069654b154ea73c777a
James <jdsv650@yahoo.com>
2016-03-25 23:54:08 -0400

    Add hundredths to Dec to inches VC, add icons

--- Conversion2/DecimalToInchesViewController.swift
+++ Conversion2/DecimalToInchesViewController.swift
@@ -15,10 +15,13 @@ class DecimalToInchesViewController: UIViewController, UIPickerViewDataSource, U
     @IBOutlet weak var num2Picker: UIPickerView!
     @IBOutlet weak var resultLabel: UILabel!
     
+    @IBOutlet weak var FeetAndInchesLabel: UILabel!
+    
      var whole = [["0","1","2","3","4","5","6","7","8","9"],
                    ["0","1","2","3","4","5","6","7","8","9"],
                    ["0","1","2","3","4","5","6","7","8","9"],
                    ["."],
+                   ["0","1","2","3","4","5","6","7","8","9"],
                    ["0","1","2","3","4","5","6","7","8","9"]]
     
     override func viewDidLoad() {
@@ -29,13 +32,16 @@ class DecimalToInchesViewController: UIViewController, UIPickerViewDataSource, U
         num1Picker.selectRow(2, inComponent: 1, animated: true)
         num1Picker.selectRow(9, inComponent: 2, animated: true)
         num1Picker.selectRow(9, inComponent: 4, animated: true)
+        num1Picker.selectRow(5, inComponent: 5, animated: true)
+
         
         // Do any additional setup after loading the view.
         num2Picker.selectRow(6, inComponent: 0, animated: true)
         num2Picker.selectRow(2, inComponent: 1, animated: true)
         num2Picker.selectRow(8, inComponent: 2, animated: true)
         num2Picker.selectRow(4, inComponent: 4, animated: true)
-        
+        num2Picker.selectRow(5, inComponent: 5, animated: true)
+
         
         //get the part after . decimalPicker.selectedRowInComponent(4)
     }
@@ -62,36 +68,42 @@ class DecimalToInchesViewController: UIViewController, UIPickerViewDataSource, U
     
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
     {
-        var numberAsInt: Int
         var decimal1AsString: String = ""
         var decimal2AsString: String = ""
         
-        //if pickerView.isEqual(num1Picker)
-       // {
         
         let number1AsString = whole[0][num1Picker.selectedRowInComponent(0)] + whole[1][num1Picker.selectedRowInComponent(1)] + whole[2][num1Picker.selectedRowInComponent(2)]
             
-        let number1AsInteger = (number1AsString as NSString).integerValue
-            decimal1AsString = "\(number1AsString).\(num1Picker.selectedRowInComponent(4))"
-       // }
-       // else
-       // {
+      //  let number1AsInteger = (number1AsString as NSString).integerValue
+            decimal1AsString = "\(number1AsString).\(num1Picker.selectedRowInComponent(4))\(num1Picker.selectedRowInComponent(5))"
+     
             let number2AsString = whole[0][num2Picker.selectedRowInComponent(0)] + whole[1][num2Picker.selectedRowInComponent(1)] + whole[2][num2Picker.selectedRowInComponent(2)]
-            let number2AsInt = (number2AsString as NSString).integerValue
-            
-            let number2AsInteger = (number2AsString as NSString).integerValue
-              decimal2AsString = "\(number2AsString).\(num2Picker.selectedRowInComponent(4))"
-       // }
         
-        var num1AsDouble = (decimal1AsString as NSString).doubleValue
-        var num2AsDouble = (decimal2AsString as NSString).doubleValue
+           // let number2AsInteger = (number2AsString as NSString).integerValue
+              decimal2AsString = "\(number2AsString).\(num2Picker.selectedRowInComponent(4))\(num2Picker.selectedRowInComponent(5))"
+    
+        
+        let num1AsDouble = (decimal1AsString as NSString).doubleValue
+        let num2AsDouble = (decimal2AsString as NSString).doubleValue
 
-        var result = num1AsDouble - num2AsDouble
+        let result = num1AsDouble - num2AsDouble
         
-        var rounded = roundToPlaces(result, places: 5)
+        let rounded = roundToPlaces(result, places: 5)
         
         resultLabel.text = "\(rounded)"
+        displayInFeetAndInches(rounded)
 
+    }
+    
+    
+    func displayInFeetAndInches(result :Double)
+    {
+        
+        let feet = Int(result)
+        let inchesAsDecimal = result - Double(feet)
+        
+        
+        FeetAndInchesLabel.text = "Feet = \(feet) Inches = \(inchesAsDecimal * 12)"
         
     }
     
